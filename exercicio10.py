48295236c30aaf352d3960e1d10e2393e4c6ccdf
Pedroliberato <143739949+Pedroliberato@users.noreply.github.com>
2023-09-26 16:49:24 -0300

    Add files via upload

---
+++ exercicio10.py
@@ -0,0 +1,14 @@
+'''
+Criar 2 tuplas e colocar os mesmo numeros nas duas
+multiplicar o indice de uma tupla com o indice de
+outra tupla.
+'''
+
+tupla1 = (1,3,2,4,5)
+tupla2 = (1,3,2,4,5)
+
+for i in range(0,len(tupla1)):
+    r1 = tupla1[i]
+for i in range(0,len(tupla2)):
+    r2 = tupla2[i]
+    print(r1*r2)
